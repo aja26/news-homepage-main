222f9c87d99cce8c46aaeba62d3fc9c6db7c74e7
Adam Alexander <adamjalexander88@gmail.com>
2022-11-13 15:48:10 +0000

    update

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const toggleMenu = document.querySelector('.mobile-menu');
 const ul = document.querySelector('ul');
+ const docBody = window.document.body;
 
 toggleMenu.addEventListener('click', () => {
     ul.classList.toggle('active');
+    docBody.classList.toggle('active');
     if(ul.classList.contains('active')){
         toggleMenu.innerHTML = `<img src="assets/images/icon-menu-close.svg" alt="menu-icon-close">`;
     } else{
